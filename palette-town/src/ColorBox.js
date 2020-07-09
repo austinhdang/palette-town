d6966cd569a0446e84b82c37713367f5bc6ec017
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-09 19:22:49 -0400

    Add copy overlay animation

--- palette-town/src/ColorBox.js
+++ palette-town/src/ColorBox.js
@@ -3,11 +3,33 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isCopied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+
+  /* Changes isCopied to true and flips back to false after 1500ms */
+  changeCopyState() {
+    this.setState({ isCopied: true }, () => {
+      setTimeout(() => this.setState({ isCopied: false }), 1500);
+    });
+  }
+
   render() {
     const { name, background } = this.props;
+    const { isCopied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className='ColorBox'>
+          <div
+            style={{ background }}
+            className={`copy-overlay ${isCopied && 'show'}`}
+          />
+          <div className={`copy-msg ${isCopied && 'show'}`}>
+            <h1>Copied!</h1>
+            <p>{background}</p>
+          </div>
           <div className='copy-container'>
             <div className='ColorBox-content'>
               <span>{name}</span>
