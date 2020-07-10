1df647ae311d8518a8ea9c71cf97d04018df0f93
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-10 00:33:37 -0400

    Add format select and Material UI

--- palette-town/src/Navbar.js
+++ palette-town/src/Navbar.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: 'hex' };
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+
+  changeFormat(evt) {
+    this.setState({ format: evt.target.value });
+    this.props.changeFormat(evt.target.value);
+  }
+
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className='Navbar'>
         <div className='Navbar-logo'>
@@ -33,6 +47,13 @@ class Navbar extends Component {
               }}
             />
           </div>
+        </div>
+        <div className='select-container'>
+          <Select value={format} onChange={this.changeFormat}>
+            <MenuItem value='hex'>HEX - #1243EF</MenuItem>
+            <MenuItem value='rgb'>RGB - (255,255,255)</MenuItem>
+            <MenuItem value='rgba'>RGBA - (12,12,12,1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
