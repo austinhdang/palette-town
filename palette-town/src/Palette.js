75ab4c3d16c363adf7bd815d2f08ae6515d91924
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-10 20:41:23 -0400

    Add palette footer, disable scrolling

--- palette-town/src/Palette.js
+++ palette-town/src/Palette.js
@@ -22,10 +22,10 @@ class Palette extends Component {
   }
 
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format, isDiffFormat } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ));
     return (
       <div className='Palette'>
@@ -44,7 +44,10 @@ class Palette extends Component {
             <h1>Changing color format!</h1>
           </div>
         </div>
-        {/* Footer goes here */}
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     );
   }
