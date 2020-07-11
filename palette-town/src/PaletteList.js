2a56f70b107ea42783142dca08eb6df87da2ccf4
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-10 22:59:47 -0400

    Add MiniPalette and start styling

--- palette-town/src/PaletteList.js
+++ palette-town/src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 class PaletteList extends Component {
   render() {
@@ -7,11 +8,7 @@ class PaletteList extends Component {
     return (
       <div>
         <h1>Palette Town</h1>
-        {palettes.map((palette) => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
-        ))}
+        {palettes.map((palette) => <MiniPalette {...palette} />)}
       </div>
     );
   }
