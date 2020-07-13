1cc11a037df879b8ec72e766a4b39f040e84971a
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-12 22:22:30 -0400

    Move out styles

--- palette-town/src/Palette.js
+++ palette-town/src/Palette.js
@@ -3,67 +3,7 @@ import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
 import { withStyles } from '@material-ui/styles';
-import './Palette.css';
-
-const styles = {
-  Palette: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    overflow: 'hidden',
-  },
-  colors: {
-    height: '90%',
-  },
-  formatOverlay: {
-    display: 'none',
-    opacity: '0',
-    zIndex: '0',
-    width: '100%',
-    height: '100%',
-    transition: 'transform 0.6s ease-in-out',
-    transform: 'scale(0.1)',
-  },
-  showOverlay: {
-    display: 'flex',
-    opacity: '1',
-    transform: 'scale(50)',
-    zIndex: '2',
-    position: 'absolute',
-  },
-  formatMessage: {
-    position: 'fixed',
-    left: '0',
-    right: '0',
-    top: '0',
-    bottom: '100px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '2rem',
-    transform: 'scale(0.1)',
-    opacity: '0',
-    color: 'white',
-    '& h1': {
-      fontWeight: '400',
-      textShadow: '1px 2px black',
-      background: 'rgba(255, 255, 255, 0.2)',
-      width: '100%',
-      textAlign: 'center',
-      marginBottom: '0',
-      padding: '1rem',
-      textTransform: 'uppercase',
-    },
-  },
-  showMessage: {
-    opacity: '1',
-    transform: 'scale(1)',
-    zIndex: '3',
-    transition: 'all 0.4s ease-in-out',
-    transitionDelay: '0.1s',
-  },
-};
+import styles from './styles/PaletteStyles';
 
 class Palette extends Component {
   constructor(props) {
