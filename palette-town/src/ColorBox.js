faf4f346fe23f1ff2df2018aecfa17251ce1fcf9
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-12 21:21:11 -0400

    Refactor ColorBox styles

--- palette-town/src/ColorBox.js
+++ palette-town/src/ColorBox.js
@@ -2,7 +2,68 @@ import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
 import chroma from 'chroma-js';
+import { withStyles } from '@material-ui/styles';
 import './ColorBox.css';
+
+const styles = {
+  ColorBox: {
+    width: '20%',
+    height: (props) => (props.isFullPalette ? '25%' : '50%'),
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-3.5px',
+    '&:hover button': {
+      opacity: '1',
+      transition: 'all 0.3s ease-in',
+      cursor: 'pointer',
+    },
+  },
+  copyText: {
+    color: (props) =>
+      chroma.contrast(props.background, 'white') < 4.5 ? 'black' : 'white',
+  },
+  colorName: {
+    color: (props) =>
+      chroma.contrast(props.background, 'white') < 4.5 ? 'black' : 'white',
+  },
+  seeMore: {
+    color: (props) =>
+      chroma.contrast(props.background, 'white') < 4.5 ? 'black' : 'white',
+    background: 'rgba(255, 255, 255, 0.3)',
+    position: 'absolute',
+    border: 'none',
+    right: '0',
+    bottom: '0',
+    width: '60px',
+    height: '30px',
+    textAlign: 'center',
+    lineHeight: '30px',
+    textTransform: 'uppercase',
+  },
+  copyButton: {
+    color: (props) =>
+      chroma.contrast(props.background, 'white') < 4.5 ? 'black' : 'white',
+    width: '100px',
+    height: '30px',
+    position: 'absolute',
+    display: 'inline-block',
+    top: '50%',
+    left: '50%',
+    marginLeft: '-50px',
+    marginTop: '-15px',
+    textAlign: 'center',
+    outline: 'none',
+    background: 'rgba(255, 255, 255, 0.3)',
+    fontSize: '1rem',
+    lineHeight: '30px',
+    textTransform: 'uppercase',
+    border: 'none',
+    textDecoration: 'none',
+    opacity: 0,
+  },
+};
 
 class ColorBox extends Component {
   constructor(props) {
@@ -18,30 +79,28 @@ class ColorBox extends Component {
   }
 
   render() {
-    const { name, background, moreUrl, showLink } = this.props;
+    const { name, background, moreUrl, isFullPalette, classes } = this.props;
     const { isCopied } = this.state;
-    const textColor =
-      chroma.contrast(background, 'white') < 4.5 ? 'dark-text' : 'light-text';
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div style={{ background }} className='ColorBox'>
+        <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
             className={`copy-overlay ${isCopied && 'show'}`}
           />
           <div className={`copy-msg ${isCopied && 'show'}`}>
             <h1>Copied!</h1>
-            <p className={textColor}>{background}</p>
+            <p className={classes.copyText}>{background}</p>
           </div>
           <div className='copy-container'>
             <div className='ColorBox-content'>
-              <span className={textColor}>{name}</span>
+              <span className={classes.colorName}>{name}</span>
             </div>
-            <button className={`copy-button ${textColor}`}>Copy</button>
+            <button className={classes.copyButton}>Copy</button>
           </div>
-          {showLink && (
+          {isFullPalette && (
             <Link to={moreUrl} onClick={(evt) => evt.stopPropagation()}>
-              <span className={`see-more ${textColor}`}>More</span>
+              <span className={classes.seeMore}>More</span>
             </Link>
           )}
         </div>
@@ -50,4 +109,4 @@ class ColorBox extends Component {
   }
 }
 
-export default ColorBox;
+export default withStyles(styles)(ColorBox);
--- palette-town/src/Palette.js
+++ palette-town/src/Palette.js
@@ -33,7 +33,7 @@ class Palette extends Component {
         id={color.id}
         paletteId={id}
         moreUrl={`/palette/${id}/${color.id}`}
-        showLink
+        isFullPalette
       />
     ));
     return (
