55a87251dc6425e555f5f06cdf79d307f45986d0
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-12 22:03:29 -0400

    Refactor Palette Styles

--- palette-town/src/Palette.js
+++ palette-town/src/Palette.js
@@ -1,8 +1,69 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import './Palette.css';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+import { withStyles } from '@material-ui/styles';
+import './Palette.css';
+
+const styles = {
+  Palette: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'hidden',
+  },
+  colors: {
+    height: '90%',
+  },
+  formatOverlay: {
+    display: 'none',
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)',
+  },
+  showOverlay: {
+    display: 'flex',
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '2',
+    position: 'absolute',
+  },
+  formatMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '100px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '2rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: 'white',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase',
+    },
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '3',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.1s',
+  },
+};
 
 class Palette extends Component {
   constructor(props) {
@@ -24,6 +85,7 @@ class Palette extends Component {
 
   render() {
     const { colors, paletteName, emoji, id } = this.props.palette;
+    const { classes } = this.props;
     const { level, format, isDiffFormat } = this.state;
     const colorBoxes = colors[level].map((color) => (
       <ColorBox
@@ -37,20 +99,24 @@ class Palette extends Component {
       />
     ));
     return (
-      <div className='Palette'>
+      <div className={classes.Palette}>
         <Navbar
           level={level}
           changeLevel={this.changeLevel}
           changeFormat={this.changeFormat}
           isAllColors
         />
-        <div className='Palette-colors'>
+        <div className={classes.colors}>
           {colorBoxes}
           <div
             style={{ background: '#1ba619' }}
-            className={`format-overlay ${isDiffFormat && 'show'}`}
+            className={`${classes.formatOverlay} ${isDiffFormat &&
+              classes.showOverlay}`}
           />
-          <div className={`format-msg ${isDiffFormat && 'show'}`}>
+          <div
+            className={`${classes.formatMessage} ${isDiffFormat &&
+              classes.showMessage}`}
+          >
             <h1>Changing color format!</h1>
           </div>
         </div>
@@ -60,4 +126,4 @@ class Palette extends Component {
   }
 }
 
-export default Palette;
+export default withStyles(styles)(Palette);
--- palette-town/src/SingleColorPalette.js
+++ palette-town/src/SingleColorPalette.js
@@ -3,6 +3,97 @@ import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import ColorBox from './ColorBox';
 import PaletteFooter from './PaletteFooter';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  Palette: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'hidden',
+  },
+  colors: {
+    height: '90%',
+  },
+  back: {
+    width: '20%',
+    height: '50%',
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-3.5px',
+    opacity: '1',
+    backgroundColor: 'black',
+    '& a': {
+      color: 'white',
+      width: '100px',
+      height: '30px',
+      position: 'absolute',
+      display: 'inline-block',
+      top: '50%',
+      left: '50%',
+      marginLeft: '-50px',
+      marginTop: '-15px',
+      textAlign: 'center',
+      outline: 'none',
+      background: 'rgba(255, 255, 255, 0.3)',
+      fontSize: '1rem',
+      lineHeight: '30px',
+      textTransform: 'uppercase',
+      border: 'none',
+      textDecoration: 'none',
+    },
+  },
+  formatOverlay: {
+    display: 'none',
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)',
+  },
+  showOverlay: {
+    display: 'flex',
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '2',
+    position: 'absolute',
+  },
+  formatMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '100px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '2rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: 'white',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase',
+    },
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '3',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.1s',
+  },
+};
 
 class SingleColorPalette extends Component {
   constructor(props) {
@@ -31,6 +122,7 @@ class SingleColorPalette extends Component {
 
   render() {
     const { paletteName, emoji, id } = this.props.palette;
+    const { classes } = this.props;
     const { format, isDiffFormat } = this.state;
     const colorBoxes = this._shades.map((color) => (
       <ColorBox
@@ -41,21 +133,23 @@ class SingleColorPalette extends Component {
       />
     ));
     return (
-      <div className='SingleColorPalette Palette'>
+      <div className={classes.Palette}>
         <Navbar changeFormat={this.changeFormat} isAllColors={false} />
-        <div className='Palette-colors'>
+        <div className={classes.colors}>
           {colorBoxes}
-          <div className='back ColorBox'>
-            <Link to={`/palette/${id}`} className='back-button'>
-              BACK
-            </Link>
+          <div className={classes.back}>
+            <Link to={`/palette/${id}`}>BACK</Link>
           </div>
         </div>
         <div
           style={{ background: '#1ba619' }}
-          className={`format-overlay ${isDiffFormat && 'show'}`}
+          className={`${classes.formatOverlay} ${isDiffFormat &&
+            classes.showOverlay}`}
         />
-        <div className={`format-msg ${isDiffFormat && 'show'}`}>
+        <div
+          className={`${classes.formatMessage} ${isDiffFormat &&
+            classes.showMessage}`}
+        >
           <h1>Changing color format!</h1>
         </div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
@@ -64,4 +158,4 @@ class SingleColorPalette extends Component {
   }
 }
 
-export default SingleColorPalette;
+export default withStyles(styles)(SingleColorPalette);
