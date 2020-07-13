43d74c3dac12a992e70afac5a1eb5deba84e5b69
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-12 22:40:56 -0400

    Refactor Navbar styles and remove CSS

--- palette-town/src/Navbar.js
+++ palette-town/src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -7,7 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
+import styles from './styles/NavbarStyles';
 
 class Navbar extends Component {
   constructor(props) {
@@ -29,17 +30,17 @@ class Navbar extends Component {
   }
 
   render() {
-    const { level, changeLevel, isAllColors } = this.props;
+    const { level, changeLevel, isAllColors, classes } = this.props;
     const { format, open } = this.state;
     return (
-      <header className='Navbar'>
-        <div className='Navbar-logo'>
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to='/'>palettetown</Link>
         </div>
         {isAllColors && (
-          <div className='slider-container'>
+          <div>
             <span>Level: {level}</span>
-            <div className='slider'>
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -61,7 +62,7 @@ class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className='select-container'>
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.changeFormat}>
             <MenuItem value='hex'>HEX - #1243EF</MenuItem>
             <MenuItem value='rgb'>RGB - (255,255,255)</MenuItem>
@@ -97,4 +98,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
---
+++ palette-town/src/styles/NavbarStyles.js
@@ -0,0 +1,31 @@
+export default {
+  Navbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: '6vh',
+  },
+  logo: {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '15px',
+    padding: '0 13px',
+    height: '100%',
+    fontSize: '22px',
+    backgroundColor: '#eceff1',
+    fontFamily: 'Roboto',
+    '& a': {
+      textDecoration: 'none',
+      color: 'black',
+    },
+  },
+  slider: {
+    width: '340px',
+    margin: '0 10px',
+    display: 'inline-block',
+  },
+  selectContainer: {
+    marginLeft: 'auto',
+    marginRight: '1rem',
+  },
+};
