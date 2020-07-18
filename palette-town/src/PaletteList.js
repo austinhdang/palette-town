9a537e8b8cef779365175260beb5d22ffd6a33b0
Austin <65687945+austinhdang@users.noreply.github.com>
2020-07-18 18:41:36 -0400

    Change PaletteList to functional component

--- palette-town/src/PaletteList.js
+++ palette-town/src/PaletteList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
@@ -6,39 +6,38 @@ import { withStyles } from '@material-ui/styles';
 import AddIcon from '@material-ui/icons/Add';
 import styles from './styles/PaletteListStyles';
 
-class PaletteList extends Component {
-  goToPalette(id) {
-    this.props.history.push(`/palette/${id}`);
-  }
+function PaletteList(props) {
+  const { history, palettes, classes, deletePalette } = props;
 
-  render() {
-    const { palettes, classes, deletePalette } = this.props;
-    return (
-      <div className={classes.root}>
-        <div className={classes.container}>
-          <nav className={classes.nav}>
-            <h1 className={classes.heading}>Palette Town</h1>
-            <Link to='/palette/new' className={classes.link}>
-              New Palette <AddIcon />
-            </Link>
-          </nav>
-          <TransitionGroup className={classes.palettes}>
-            {palettes.map((palette) => (
-              <CSSTransition key={palette.id} classNames='fade' timeout={500}>
-                <MiniPalette
-                  {...palette}
-                  handleClick={() => this.goToPalette(palette.id)}
-                  handleDelete={deletePalette}
-                  key={palette.id}
-                  id={palette.id}
-                />
-              </CSSTransition>
-            ))}
-          </TransitionGroup>
-        </div>
+  const goToPalette = (id) => {
+    history.push(`/palette/${id}`);
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1 className={classes.heading}>Palette Town</h1>
+          <Link to='/palette/new' className={classes.link}>
+            New Palette <AddIcon />
+          </Link>
+        </nav>
+        <TransitionGroup className={classes.palettes}>
+          {palettes.map((palette) => (
+            <CSSTransition key={palette.id} classNames='fade' timeout={500}>
+              <MiniPalette
+                {...palette}
+                handleClick={() => goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
+              />
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withStyles(styles)(PaletteList);
